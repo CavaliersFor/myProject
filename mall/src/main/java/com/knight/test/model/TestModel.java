12f7fa17205ecd7748c4734fea4f678209f73494
yin <yinqijs@126.com>
2016-10-20 12:00:39 +0800

    mall第一次提交

---
+++ mall/src/main/java/com/knight/test/model/TestModel.java
@@ -0,0 +1,13 @@
+package com.knight.test.model;
+
+public class TestModel {
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
