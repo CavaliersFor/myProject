6d68ed551a145a6988044ee8c52602e056052e32
yin <yinqijs@126.com>
2016-10-25 10:21:12 +0800

    修改文件

--- mall/src/main/java/com/knight/mall/interceptor/WholeInterceptor.java
+++ mall/src/main/java/com/knight/mall/interceptor/WholeInterceptor.java
@@ -10,10 +10,10 @@ public class WholeInterceptor implements HandlerInterceptor {
 
 	private static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(WholeInterceptor.class);
 
+
 	// 在控制器方法调用前执行
 	// 返回值为是否中断，true,表示继续执行（下一个拦截器或处理器）
 	// false则会中断后续的所有操作，所以我们需要使用response来响应请求
-	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 
@@ -21,7 +21,6 @@ public class WholeInterceptor implements HandlerInterceptor {
 	}
 
 	// 在控制器方法调用后，解析视图前调用，我们可以对视图和模型做进一步渲染或修改
-	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 
@@ -37,10 +36,10 @@ public class WholeInterceptor implements HandlerInterceptor {
 	}
 
 	// 整个请求完成，即视图渲染结束后调用，这个时候可以做些资源清理工作，或日志记录等
-	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
 
 	}
+
 
 }
