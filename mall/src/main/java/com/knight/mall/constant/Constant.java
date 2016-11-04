9a91392bcf5d7a30d110f9e1d8c63ce43b51a81c
uy111 <yinqijs@126.com>
2016-11-04 15:08:46 +0800

    代码提交

--- mall/src/main/java/com/knight/mall/constant/Constant.java
+++ mall/src/main/java/com/knight/mall/constant/Constant.java
@@ -10,16 +10,15 @@ public interface Constant {
 
 	public interface REGION {
 		public interface REGIONTYPE {
-			// 地区类型，0：国家，1： 省份/直辖市，2：市，3：县/区
-			// 地区类型，0：国家，
-			public static final Integer COUNTRY = 0;
-
-			// 1： 省份/直辖市
-			public static final Integer PROVINCE = 1;
-			// 2：市
-			public static final Integer CITY = 2;
-			// 3：县/区
-			public static final Integer AREA = 3;
+			// 地区类型，1：国家，2： 省份/直辖市，3：市，4：县/区
+			// 地区类型，1：国家，
+			public static final Integer COUNTRY = 1;
+			// 2： 省份/直辖市
+			public static final Integer PROVINCE = 2;
+			// 3：市
+			public static final Integer CITY = 3;
+			// 4：县/区
+			public static final Integer AREA = 4;
 		}
 	}
 
